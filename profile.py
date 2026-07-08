4be65c8d123e1a5a59a4bd3b5e458ed07881940d
Amaan Abdullah <amaanabdullah.20018843@gmail.com>
2026-07-09 02:20:45 +0530

    Add files via upload
    day 1

---
+++ profile.py
@@ -0,0 +1,11 @@
+# EXERCISE 1 :- 
+print("-----AMAAN ABDULLAH COMPLETE PROFILE----")
+name = "amaan abdullah"
+age = 19
+company = "ServiceLabs3"
+city = "Kanpur"
+dream = "BECOME one of the youngest billionaire"
+target = "become ALLAH KA BANDA"
+
+# Ek hi line mein f-string ko laya gaya hai aur quotes sahi kiye hain
+print(f"Mera naam {name} hai. Mai {age} saal ka hu. Meri company {company} is gonna be one of the largest tech research based service company. I live in {city}. MY dream is to {dream}. MY forever target is to {target}.")
